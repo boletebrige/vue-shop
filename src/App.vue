a640eed4f9e13c45328c99f322e77ec37f73d077
boletebrige <josip.ravas.broj@gmail.com>
2018-01-25 13:56:40 +0100

    categories

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="app">
     <h1 class="shop-title">Digital<br><span>Retail</span></h1>
-    <div class="categories">
-      <span class="fi flaticon-shirt active"></span>
-      <span class="fi flaticon-mug"></span>
-      <span class="fi flaticon-sticker"></span>
-    </div>
-    <div class="divider"></div>
     <router-view/>
     <h1 class="ribbon">
       <span class="ribbon-content">&copy; Copyright 2017.</span>
@@ -23,51 +17,4 @@ export default {
 
 <style lang="scss">
 @import './styles/styles';
-
-.ribbon {
-  font-size: 16px !important;
-  font-family: 'Roboto', sans-serif;
-  font-weight: 300;
-  position: relative;
-  background: #ff3a7d;
-  color: #fff;
-  text-align: center;
-  padding: 1em 2em;
-  margin: 2em auto 3em;
-  margin-top: -40px;
-}
-.ribbon:before, .ribbon:after {
-  content: "";
-  position: absolute;
-  display: block;
-  bottom: -1em;
-  border: 1.5em solid #ed3775;
-  z-index: -1;
-}
-.ribbon:before {
-  left: -2em;
-  border-right-width: 1.5em;
-  border-left-color: transparent;
-}
-.ribbon:after {
-  right: -2em;
-  border-left-width: 1.5em;
-  border-right-color: transparent;
-}
-.ribbon .ribbon-content:before, .ribbon .ribbon-content:after {
-  content: "";
-  position: absolute;
-  display: block;
-  border-style: solid;
-  border-color: #804f7c transparent transparent transparent;
-  bottom: -1em;
-}
-.ribbon .ribbon-content:before {
-  left: 0;
-  border-width: 1em 0 0 1em;
-}
-.ribbon .ribbon-content:after {
-  right: 0;
-  border-width: 1em 1em 0 0;
-}
 </style>
---
+++ src/data/products.js
@@ -0,0 +1,42 @@
+export const TSHIRTS = [
+  { id: 0, model_name: '', image: './static/shirt.png', price: '$20' },
+  { id: 2, model_name: '', image: './static/shirt.png', price: '$20' },
+  { id: 1, model_name: '', image: './static/shirt.png', price: '$20' },
+  { id: 4, model_name: '', image: './static/shirt.png', price: '$20' },
+  { id: 3, model_name: '', image: './static/shirt.png', price: '$20' },
+  { id: 6, model_name: '', image: './static/shirt.png', price: '$20' },
+  { id: 5, model_name: '', image: './static/shirt.png', price: '$20' },
+  { id: 8, model_name: '', image: './static/shirt.png', price: '$20' },
+  { id: 7, model_name: '', image: './static/shirt.png', price: '$20' },
+  { id: 10, model_name: '', image: 'https://upload.wikimedia.org/wikipedia/commons/0/0c/Crew_neck_T-shirt.jpg', price: '$20' },
+  { id: 9, model_name: '', image: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Blue_Tshirt.jpg', price: '$20' },
+  { id: 11, model_name: '', image: 'https://upload.wikimedia.org/wikipedia/commons/0/0c/Crew_neck_T-shirt.jpg', price: '$20' }
+]
+export const MUGS = [
+  { id: 0, model_name: '', image: 'https://angularspree-new.herokuapp.com//spree/products/27/large/ror_mug.jpeg?1516188407', price: '$5' },
+  { id: 2, model_name: '', image: 'https://angularspree-new.herokuapp.com//spree/products/27/large/ror_mug.jpeg?1516188407', price: '$5' },
+  { id: 1, model_name: '', image: 'https://angularspree-new.herokuapp.com//spree/products/27/large/ror_mug.jpeg?1516188407', price: '$5' },
+  { id: 4, model_name: '', image: 'https://angularspree-new.herokuapp.com//spree/products/27/large/ror_mug.jpeg?1516188407', price: '$5' },
+  { id: 3, model_name: '', image: 'https://angularspree-new.herokuapp.com//spree/products/27/large/ror_mug.jpeg?1516188407', price: '$5' },
+  { id: 6, model_name: '', image: 'https://angularspree-new.herokuapp.com//spree/products/27/large/ror_mug.jpeg?1516188407', price: '$5' },
+  { id: 5, model_name: '', image: 'https://angularspree-new.herokuapp.com//spree/products/27/large/ror_mug.jpeg?1516188407', price: '$5' },
+  { id: 8, model_name: '', image: 'https://angularspree-new.herokuapp.com//spree/products/27/large/ror_mug.jpeg?1516188407', price: '$5' },
+  { id: 7, model_name: '', image: 'https://angularspree-new.herokuapp.com//spree/products/27/large/ror_mug.jpeg?1516188407', price: '$5' },
+  { id: 10, model_name: '', image: 'https://upload.wikimedia.org/wikipedia/commons/0/0c/Crew_neck_T-shirt.jpg', price: '$20' },
+  { id: 9, model_name: '', image: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Blue_Tshirt.jpg', price: '$20' },
+  { id: 11, model_name: '', image: 'https://upload.wikimedia.org/wikipedia/commons/0/0c/Crew_neck_T-shirt.jpg', price: '$20' }
+]
+export const STICKERS = [
+  { id: 0, model_name: '', image: 'https://git-scm.com/images/logos/downloads/Git-Logo-2Color.png', price: '$1' },
+  { id: 2, model_name: '', image: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg', price: '$1' },
+  { id: 1, model_name: '', image: 'https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg', price: '$1' },
+  { id: 4, model_name: '', image: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Vue.png', price: '$1' },
+  { id: 3, model_name: '', image: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg', price: '$1' },
+  { id: 6, model_name: '', image: 'https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg', price: '$1' },
+  { id: 5, model_name: '', image: 'https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg', price: '$1' },
+  { id: 8, model_name: '', image: 'https://git-scm.com/images/logos/downloads/Git-Logo-2Color.png', price: '$1' },
+  { id: 7, model_name: '', image: 'https://git-scm.com/images/logos/downloads/Git-Logo-2Color.png', price: '$1' },
+  { id: 10, model_name: '', image: 'https://upload.wikimedia.org/wikipedia/commons/0/0c/Crew_neck_T-shirt.jpg', price: '$20' },
+  { id: 9, model_name: '', image: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Blue_Tshirt.jpg', price: '$20' },
+  { id: 11, model_name: '', image: 'https://upload.wikimedia.org/wikipedia/commons/0/0c/Crew_neck_T-shirt.jpg', price: '$20' }
+]
